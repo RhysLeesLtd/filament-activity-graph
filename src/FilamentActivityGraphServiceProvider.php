0ed93f96bdc614dfc29c7ccc3fccb38208a1614f
RhysLees <43909932+RhysLees@users.noreply.github.com>
2025-08-02 21:20:07 +0000

    Fix styling

--- src/FilamentActivityGraphServiceProvider.php
+++ src/FilamentActivityGraphServiceProvider.php
@@ -10,11 +10,11 @@ use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
+use RhysLeesLtd\FilamentActivityGraph\Commands\FilamentActivityGraphCommand;
+use RhysLeesLtd\FilamentActivityGraph\Testing\TestsFilamentActivityGraph;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use RhysLeesLtd\FilamentActivityGraph\Commands\FilamentActivityGraphCommand;
-use RhysLeesLtd\FilamentActivityGraph\Testing\TestsFilamentActivityGraph;
 
 class FilamentActivityGraphServiceProvider extends PackageServiceProvider
 {
